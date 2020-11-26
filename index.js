f113caf186579584167fc15d72f3a5374ef5317d
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2020-11-26 20:35:06 +0000

    Working with paths.

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 console.log('-----------');
 console.log('Blue print script');
 const shell = require('child_process').execSync;
+const path = require('path');
 
 const src = `./src`;
 const dist = `./path/dist`;
 
-console.log(__dirname);
+console.log(path.resolve());
 shell(`mkdir -p ${dist}`);
 shell(`cp -r ${src}/* ${dist}`);
 
