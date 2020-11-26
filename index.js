703332512ff83ca6038b539b04bec8bbefbed5dd
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2020-11-26 20:26:20 +0000

    POC Test

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 console.log('-----------');
 console.log('Blue print script');
+const shell = require('child_process').execSync;
+
+const src = `./src`;
+const dist = `./path/dist`;
+
+console.log(__dirname);
+shell(`mkdir -p ${dist}`);
+shell(`cp -r ${src}/* ${dist}`);
+
