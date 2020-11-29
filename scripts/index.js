c1c1d06d4e3b0eb316b410097601fbe460db71fc
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2020-11-29 15:59:32 +0000

    Copy the correct framework files across.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const shell = require('child_process').execSync;
+const { exec } = require('child_process');
 const path = require('path');
 const commander = require('commander');
 const chalk = require('chalk');
@@ -11,61 +11,75 @@ const scriptName = 'Create City Blueprint';
 const semver = currentNodeVersion.split('.');
 const major = semver[0];
 
-if (major < 12) {
-    console.error(
-        `You are running Node ${currentNodeVersion}.\n` +
-        `${scriptName} requires Node 12 or higher. \n` +
-        'Please update your version of Node.'
-    );
-    process.exit(1);
+function shCommand(cmd) {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(stdout, stderr);
+                resolve({});
+            }
+        });
+    });
 }
 
-let projectName = '';
-const program = new commander.Command(packageJson.name)
-    .version(packageJson.version)
-    .arguments('<project-directory>')
-    .usage(`${chalk.green('<project-directory>')}`)
-    .action(name => {
-        projectName = name;
-    })
-    .on('--help', () => {
-        console.log();
-        console.log(
-            `Only ${chalk.green('<project-directory>')} is required.`
+(async () => {
+    if (major < 12) {
+        console.error(
+            `You are running Node ${currentNodeVersion}.\n` +
+            `${scriptName} requires Node 12 or higher. \n` +
+            'Please update your version of Node.'
         );
-        console.log();
-    })
-    .parse(process.argv);
+        process.exit(1);
+    }
 
-// Set path to base directory and project level directory
-const baseDir = path.resolve();
-const rootDir = path.resolve(projectName);
+    let projectName = '';
+    const program = new commander.Command(packageJson.name)
+        .version(packageJson.version)
+        .arguments('<project-directory>')
+        .usage(`${chalk.green('<project-directory>')}`)
+        .action(name => {
+            projectName = name;
+        })
+        .on('--help', () => {
+            console.log();
+            console.log(
+                `Only ${chalk.green('<project-directory>')} is required.`
+            );
+            console.log();
+        })
+        .parse(process.argv);
 
-console.log(
-    `Creating new project directory \n ${chalk.green(`${projectName}`)}  =>  ${chalk.cyan(`${baseDir}`)}`
-);
+    // Set path to base directory and project level directory
+    const baseDir = path.resolve();
+    const rootDir = path.resolve(projectName);
 
-try {
-    shell(`mkdir "${rootDir}"`);
-} catch (e) {
-    console.error(
-        `${chalk.bgRed('ERR')}: while attempting to make project directory.`
+    console.log(
+        `Creating new project directory \n ${chalk.green(`${projectName}`)}  =>  ${chalk.cyan(`${baseDir}`)}`
     );
-    process.exit(1);
-}
-console.log(
-    `Created ${chalk.green(`${rootDir}`)} without error.`
-);
-try{
-    shell(`cp -r ${src} "${rootDir}"`);
-} catch (e) {
-    console.error(
-        `${chalk.bgRed('ERR')}: while attempting to setup framework.`
-    );
-    process.exit(1);
-}
-console.log(
-    `Created city framework version ${chalk.green(`${packageJson.version}`)} successfully in ${chalk.green(`${rootDir}`)}`
-);
+
+    await shCommand(`mkdir "${rootDir}"`).then(() => {
+        console.log(
+            `Created ${chalk.green(`${rootDir}`)} without error.`
+        );
+    }).catch(() => {
+        console.error(
+            `${chalk.bgRed('ERR')}: while attempting to make project directory.`
+        );
+        process.exit(1);
+    });
+    const srcFiles = path.resolve(__dirname, '../src');
+    await shCommand(`cp -r "${srcFiles}/." "${rootDir}"`).then(() => {
+        console.log(
+            `Created city framework version ${chalk.green(`${packageJson.version}`)} successfully in ${chalk.green(`${rootDir}`)}`
+        );
+    }).catch((err) => {
+        console.error(
+            `${chalk.bgRed('ERR')}: while attempting to setup framework.`
+        );
+        process.exit(1);
+    });
+})();
 
 
